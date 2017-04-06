8dcda9229082782b882b4bf7c59c482efa011aa3
Maledor <josh.r.cameron@gmail.com>
2017-04-05 20:43:51 -0400

    created all controllers (may not need them all) and veiws and added the controllers to index.html.

--- public/js/routes.js
+++ public/js/routes.js
@@ -6,9 +6,36 @@
 
   function RouterConfig($routeProvider){
     $routeProvider
-    .when()
-
-
-
-  }
+    .when('/', {
+          controller: 'HomeController',
+          templateUrl: 'html/views/home.html'
+        })
+        .when('/aboutus', {
+          controller: 'AboutUsController',
+          templateUrl: 'html/views/aboutus.html'
+        })
+        .when('/programs', {
+          controller: 'ProgramsController',
+          templateUrl: 'html/views/programs.html'
+        })
+        .when('/events', {
+          controller: 'EventsController',
+          templateUrl: 'html/views/events.html'
+        })
+        .when('/contactus', {
+          controller: 'ContactUsController',
+          templateUrl: 'html/views/contactus.html'
+        })
+        .when('/fooddrives', {
+          controller: 'FoodDrivesController',
+          templateUrl: 'html/views/fooddrives.html'
+        })
+        .when('/wishlist', {
+          controller: 'WishListController',
+          templateUrl: 'html/views/wishlist.html'
+        })
+          .otherwise({
+            redirectTo: '/'
+          });
+    }
 }());
